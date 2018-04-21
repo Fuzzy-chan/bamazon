eebc306799199e3a96dbb3e09fd73df56482c289
unknown <Fuzzych4an@gmail.com>
2018-04-21 02:00:01 -0400

    psudo supervisor

---
+++ bamazonSupervisor.js
@@ -0,0 +1,5 @@
+//I unfortunately ran out of time to do this one. If I finish the next homework early I will revisit this.
+
+//To psudo code this portion, I would add the appropriate fields to the products table. Then I would create and populate the departments table. From there I would join the two based on departments names and use those values to calculate the total profit.
+
+//In my head, it sounds pretty easy, but I imagine grouping and joining will be a lot more difficult than I want it to be. I learned in the previous activities that aliases will help reference things more easily when the time comes.
